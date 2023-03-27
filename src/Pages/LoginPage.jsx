85829b7254f7c52a3b2a96347d4950163c8fe554
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-27 15:30:08 -0600

    termiando login funcionaldiad ?

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,5 +1,7 @@
+import './LoginPage.css'
 
 export const LoginPage = () => {
+
   return (
     <section className="loginApp">
       <header className="w-96 h-96 bg-gray-400 rounded-lg">
@@ -12,7 +14,43 @@ export const LoginPage = () => {
             <button className="w-6 text-center bg-red-600 rounded-tr-md pb-1">x</button>
           </div>
         </div>
+
+        <div className="logo">
+          <img src="logo.png" alt="logo" />
+          <div>
+            <p className='title'>Program®</p>
+            <span>powered by gane</span>
+          </div>
+        </div>
+
+        <form className="flex flex-col items-center gap-2" action="" method="post">
+
+          <div className="flex justify-between w-64">
+            <label>Usuario:</label>
+            <input className="w-40 h-7 rounded-md outline-none px-1" type="text" />
+          </div>
+
+          <div className="flex justify-between w-64">
+            <label>Contraseña:</label>
+            <input className="w-40 h-7 rounded-md outline-none px-1" type="text" />
+          </div>
+
+          <div className="flex justify-between w-64">
+            <label>Conexión</label>
+            <select className="w-40" name="SELECT">
+              <option value="val1">DEFECTO</option>
+            </select>
+          </div>
+
+          <div className="flex justify-between w-64">
+            <label></label>
+            <button className='bg-red-600 w-40 h-9 rounded-lg text-white font-semibold text-md' type='submit'>Ingresar</button>
+          </div>
+
+        </form>
+
       </header>
-    </section>
+
+    </section >
   )
 }

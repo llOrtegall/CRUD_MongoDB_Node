d025e4e60b236a8b8133a0777276154e7868812c
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-18 19:08:26 -0500

    aplicando primeros estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 function App (){
   return(
-    <h1>Hello World </h1>
+    <article>
+      <header>
+        <img src="https://unavatar.io/llortegall" alt="foto" />
+        <div>
+          <strong>Ivan Ortega Garzon</strong>
+          <span>@llortegall</span>
+        </div>
+      </header>
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+    </article>
   )
 }
 

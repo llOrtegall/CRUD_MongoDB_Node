81512fa473df1a428b7dac6bd10c2a4e8a9548df
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-18 20:07:40 -0500

    concepto de children aprendido

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,15 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export const App = () => {
-  const format = (userName) => `@${userName}`
-
-  const formatedUserName = (<span>@Midudev</span>)
 
   return (
     <section className='App'>
-      <TwitterFollowCard
-        formatedUserName={formatedUserName}
-        isFollowing userName='llortegall'
-        name='Ivan Ortega Garzon' />
-      <TwitterFollowCard
-        formatedUserName={formatedUserName}
-        isFollowing userName='andref'
-        name='Andres Felipe' />
-      <TwitterFollowCard
-        formatedUserName={formatedUserName}
-        isFollowing userName='goncy'
-        name='Goncy Armando' />
-      <TwitterFollowCard
-        formatedUserName={formatedUserName}
-        isFollowing userName='elonkmusk'
-        name='Elon Musk' />
+      <TwitterFollowCard isFollowing userName='midudev'>
+        Miguel Angel Duran
+      </TwitterFollowCard>
+      <TwitterFollowCard isFollowing userName='llortegall'>
+        Ivan Ortega Garzon
+      </TwitterFollowCard>
     </section>
   )
 }
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,23 +1,28 @@
-export function TwitterFollowCard({ formatedUserName, userName, name, isFollowing }) {
+export function TwitterFollowCard({ children, userName, name, isFollowing }) {
 
   const imageSrc = `https://unavatar.io/${userName}`
 
   return (
+
+    //* TODO LO QUE ESTÁ AQUÍ SON ELEMENTOS 
+
     <article className='twitter'>
       <header className='twitter-header'>
         <img className='twitter-avatar'
           src={imageSrc}
           alt="foto" />
         <div className='twitter-info'>
-          <strong>{name}</strong>
-          <span className='twitter-info-username'>{formatedUserName}</span>
+          <strong>{children}</strong>
+          <span className='twitter-info-username'>{userName}</span>
         </div>
       </header>
       <aside>
         <button className='twitter-info-button'>
-          Seguir
+          Seguir {/* <<<------ CHILDREN */}
         </button>
       </aside>
     </article>
+
+    //* TODO LO QUE ESTÁ AQUÍ SON ELEMENTOS 
   )
 }

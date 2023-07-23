10b6b29d604ba6827a8bf73670afe02c4ad9277a
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-23 16:09:01 -0500

    edit task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,10 +1,12 @@
 import Task from "../models/Task";
 
+// TODO: renderiza las tareas ya creadas
 export const renderTask = async (req, res) => {
   const tasks = await Task.find().lean();
   res.render("index", { tasks });
 };
 
+// TODO: agregando las tareas y insertandolas en la base de datos
 export const createTask = async (req, res) => {
   try {
     const tasks = Task(req.body);
@@ -12,5 +14,14 @@ export const createTask = async (req, res) => {
     res.redirect("/");
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const editTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+    res.render("edit", { task });
+  } catch (error) {
+    console.log(error.message);
   }
 };
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,25 +1,17 @@
 import { Router, request } from "express";
-import { renderTask } from "../controllers/task.controller";
+import { createTask, renderTask } from "../controllers/task.controller";
 
 const router = Router();
 
 router.get("/", renderTask);
 
-// TODO: agregando las tareas y insertandolas en la base de datos
-router.post("/tasks/add");
+router.post("/tasks/add", createTask);
 
 router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/edit/:id", async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id).lean();
-    res.render("edit", { task });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+router.get("/edit/:id");
 
 // ? utilizado para actualizar la tareas sin embargo no es buena practica hacerlo por este metodo
 router.post("/edit/:id", async (req, res) => {

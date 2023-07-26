fc618dd02cabafec68ba14ab86d574157e404786
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-26 09:22:32 -0500

    agregando or

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 
 config();
 
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/crud-mongo";
 
 export const PORT = process.env.PORT || 3000;
